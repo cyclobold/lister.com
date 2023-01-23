7eb93f94a6c3a356e4277bfffa1f47473ec0d462
Olu <theoafactor@gmail.com>
2023-01-23 18:45:34 +0100

    Added Dockerfile and db

--- includes/connection.php
+++ includes/connection.php
@@ -5,8 +5,8 @@
  */
 $database_name = 'flamy';
 $username = 'root';
-$password = 'root';
-$host = '127.0.0.1:8889';
+$password = 'password';
+$host = 'localhost:3306';
 
 $conn = mysqli_connect($host, $username, $password, $database_name) or die("Sorry, wrong database configuration");
 
